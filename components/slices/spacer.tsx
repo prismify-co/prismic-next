e27bceea67b42af49bc0314b03935efe51df2d30
Takeshi <iwatakeshi@users.noreply.github.com>
2020-06-15 21:11:39 -0600

    Update spacer.tsx

--- components/slices/spacer.tsx
+++ components/slices/spacer.tsx
@@ -4,7 +4,7 @@ import { SpacerSliceProps } from 'types/site'
 export default function SpacerSection(props: SpacerSliceProps) {
   return (
     <Box
-      sx={{ py: props?.primary?.space ? `${props?.primary?.space}rem` : 3 }}
+      sx={{ py: props?.primary?.space ? `${props?.primary?.space}` : 3 }}
     />
   )
 }
