635faa15ab562866522eb3da076fd6caa6f671e6
Takeshi Iwana <tak@prismify.co>
2020-08-08 20:40:05 -0600

    Add webpack config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-module.exports = {
-  experimental: {
-    async rewrites() {
-      return [{ source: '/sitemap.xml', destination: '/api/sitemap' }]
-    },
+const withCSS = require('@zeit/next-css')
+
+module.exports = withCSS({
+  async rewrites() {
+    return [{ source: '/sitemap.xml', destination: '/api/sitemap' }]
   },
-}
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      }
+    }
+
+    return config
+  },
+})
