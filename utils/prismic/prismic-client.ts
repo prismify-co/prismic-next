d26923dea3261b1ff5c45875d300efff96dde1bb
Takeshi Iwana <tak@prismify.co>
2020-10-28 17:34:16 -0600

    Rename prismic client to client

--- utils/prismic/prismic-client.ts
+++
@@ -1,5 +0,0 @@
-import Prismic from 'prismic-javascript'
-import { PRISMIC_API_TOKEN, PRISMIC_API_URL } from 'lib/constants'
-export default Prismic.client(PRISMIC_API_URL, {
-  accessToken: PRISMIC_API_TOKEN,
-})
